90d8cbee6196933f45a12e3b634888768d7df02f
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-10-10 21:05:34 -0400

    Add files via upload

---
+++ Models/Characters/Uruk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W8_assignment_template.Interfaces;
+
+namespace W8_assignment_template.Models.Characters
+{
+    public class Uruk : CharacterBase, ILootable
+    {
+        public string Treasure { get; set; }
+
+        public Uruk()
+        {
+        }
+
+        public Uruk(string name, string type, int level, int hp, string treasure, IRoom startingRoom) : base(name, type, level, hp, startingRoom)
+        {
+            Treasure = treasure;
+        }
+
+        public override void UniqueBehavior()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
